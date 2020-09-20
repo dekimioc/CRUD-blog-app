6726604f3eb5eb480f7560af9378ae6dfad7e402
dekimioc <dekimioc@yahoo.com>
2020-09-21 00:47:58 +0200

    Updates

---
+++ precache-manifest.35ac38b6cf866f014c5ac81903d3d92c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd6b1e2a74462103a301f04a3ebd3b0",
+    "url": "/CRUD-blog-app/index.html"
+  },
+  {
+    "revision": "9b63246aebb83970da61",
+    "url": "/CRUD-blog-app/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "0075d46abfacca4206fe",
+    "url": "/CRUD-blog-app/static/css/main.e89cdb12.chunk.css"
+  },
+  {
+    "revision": "9b63246aebb83970da61",
+    "url": "/CRUD-blog-app/static/js/2.7a07cf49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CRUD-blog-app/static/js/2.7a07cf49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0075d46abfacca4206fe",
+    "url": "/CRUD-blog-app/static/js/main.fc106437.chunk.js"
+  },
+  {
+    "revision": "84a0c767c7995ec3d219",
+    "url": "/CRUD-blog-app/static/js/runtime-main.90918f8a.js"
+  }
+]);
